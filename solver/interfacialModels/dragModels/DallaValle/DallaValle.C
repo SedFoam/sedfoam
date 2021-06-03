2e0a607c89c4cce9e5d1765c6c9cd2b6dad822d4
remichassagne <75305569+remichassagne@users.noreply.github.com>
2021-06-03 20:32:52 +0200

    Cleaning Kinetic Theory with CheckStyle ok (#66)
    * add switch to kill fluctuating kinetic energy from fluid to particles
    
    * Remove tutorial files
    
    * Preparation of Duan classes for kinetic theory
    
    * Several correction for kinetic theory, add:
    - correction for soft particles
    - correction to account for friction in the dissipation term
    - add diffusion due to concentration gradient
    
    * version 30/04
    
    * new modifs
    
    * My kinetic theory laws
    
    * Clean kinetic Theory
    
    * Descriptions in .H files of kineticTheory/
    
    * CheckStyle

---
+++ solver/interfacialModels/dragModels/DallaValle/DallaValle.C
@@ -0,0 +1,86 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "DallaValle.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(DallaValle, 0);
+
+    addToRunTimeSelectionTable
+    (
+        dragModel,
+        DallaValle,
+        dictionary
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::DallaValle::DallaValle
+(
+    const dictionary& interfaceDict,
+    const phaseModel& phasea,
+    const phaseModel& phaseb
+)
+:
+    dragModel(interfaceDict, phasea, phaseb)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::DallaValle::~DallaValle()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::DallaValle::K
+(
+    const volScalarField& Ur
+) const
+{
+    volScalarField beta(max(scalar(1) - alpha_, scalar(1e-6)));
+    volScalarField bp(pow(beta, -phasea_.hExp()-1));
+    volScalarField Re
+    (
+        max(Ur*phasea_.d()*phasea_.sF()/phaseb_.nu(), scalar(1.0e-9))
+    );
+
+    volScalarField Cds
+    (
+      0.4+24.4/Re
+    );
+
+    return 0.75*Cds*phaseb_.rho()*Ur*bp/(phasea_.d()*phasea_.sF());
+}
+
+
+// ************************************************************************* //
---
+++ solver/kineticTheoryModels/radialModel/ChialvoSundaresan/ChialvoSundaresanRadial.C
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "ChialvoSundaresanRadial.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(ChialvoSundaresanRadial, 0);
+
+    addToRunTimeSelectionTable
+    (
+        radialModel,
+        ChialvoSundaresanRadial,
+        dictionary
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::ChialvoSundaresanRadial::ChialvoSundaresanRadial(const dictionary& dict)
+:
+    radialModel(dict)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::ChialvoSundaresanRadial::~ChialvoSundaresanRadial()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::ChialvoSundaresanRadial::g0
+(
+    const volScalarField& alpha,
+    const dimensionedScalar& alphaMax
+) const
+{
+
+    return (2-alpha)/(2*pow(1-alpha, 3)) +
+     0.58*pow(alpha, 2)/pow(alphaMax-alpha, 1.5);
+}
+
+
+Foam::tmp<Foam::volScalarField> Foam::ChialvoSundaresanRadial::g0prime
+(
+    const volScalarField& alpha,
+    const dimensionedScalar& alphaMax
+) const
+{
+    return 2./alphaMax*pow(mag(1.0 - alpha/alphaMax), -3.0);
+}
+
+
+// ************************************************************************* //
