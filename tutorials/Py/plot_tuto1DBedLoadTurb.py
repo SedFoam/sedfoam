99b5758b6ee7be22140d73fadf7da4a52f185904
Julien Chauchat <julien.chauchat@univ-grenoble-alpes.fr>
2021-06-15 23:38:14 +0200

    correct/update tutos 1DBedLoadTurb, 1DSheetFlow, 2DScour, 2DChannel and rename python postproc files for consistency

--- tutorials/Py/plot_tuto1DBedLoadTurb.py
+++ tutorials/Py/plot_tuto1DBedLoadTurb.py
@@ -9,7 +9,7 @@ plt.ion()
 import matplotlib.ticker as mticker
 from matplotlib.ticker import StrMethodFormatter, NullFormatter
 from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
  
 label_size = 20
@@ -50,6 +50,7 @@ z = Y
 phi = fluidfoam.readscalar(sol, timeStep, 'alpha_a')
 vxPart = fluidfoam.readvector(sol, timeStep, 'Ua')[0]
 vxFluid = fluidfoam.readvector(sol, timeStep, 'Ub')[0]
+Theta = fluidfoam.readscalar(sol, timeStep, 'Theta')
 
 ######################
 #Plot results
@@ -62,33 +63,41 @@ plt.plot(phi, z/d, label=r'SedFoam')
 plt.xlabel(r'$\phi$', fontsize=25)
 plt.ylabel(r'$\frac{z}{d}$', fontsize=30, rotation=True, horizontalalignment='right')
 plt.grid()
+plt.ylim([-1.525, 32.025])
 plt.legend()
 
 plt.subplot(142)
 I = np.where(phiDEM>0.001)[0]
-plt.plot(vxPDEM[I], zDEM[I]/d, 'k--', label=r'DEM')
+plt.plot(vxPDEM[I], zDEM[I]/d, 'k--', label=r'Solid phase DEM')
 I = np.where(phi>0.001)[0]
-plt.plot(vxPart[I], z[I]/d, label=r'SedFoam')
-plt.xlabel(r'$v_x^p$', fontsize=25)
+plt.plot(vxPart[I], z[I]/d, label=r'Solid phase SedFoam')
+plt.plot(vxFDEM, zDEM/d, 'k-.', label=r'Fluid phase DEM')
+plt.plot(vxFluid, z/d, label=r'Fluid phase SedFoam')
+plt.xlabel(r'$v_x^p,v_x^f$', fontsize=25)
 plt.ylim([-1.525, 32.025])
 plt.grid()
 ax = plt.gca()
 ax.set_yticklabels([])
+plt.legend()
 
 plt.subplot(143)
 plt.plot(phiDEM*vxPDEM, zDEM/d, 'k--', label=r'DEM')
 plt.plot(phi*vxPart, z/d, label=r'SedFoam')
 plt.xlabel(r'$q = \phi v_x^p$', fontsize=25)
 plt.grid()
+plt.ylim([-1.525, 32.025])
 ax = plt.gca()
 ax.set_yticklabels([])
 
 plt.subplot(144)
-plt.plot(vxFDEM, zDEM/d, 'k--', label=r'DEM')
-plt.plot(vxFluid, z/d, label=r'SedFoam')
-plt.xlabel(r'$v_x^f$', fontsize=25)
+plt.plot(Theta[I], z[I]/d, label=r'SedFoam')
+plt.xlabel(r'$\Theta$', fontsize=25)
 plt.grid()
+plt.ylim([-1.525, 32.025])
 ax = plt.gca()
 ax.set_yticklabels([])
 
 plt.savefig('Figures/res_TutoBedloadTurb.png', bbox_inches='tight')
+
+plt.show(block=False)
+
