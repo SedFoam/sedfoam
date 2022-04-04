30ffa4965c04e859b4f476ad44ff69bcdd6f2c17
puigmontella <eduard.puig-montella@univ-grenoble-alpes.fr>
2022-04-04 13:15:49 +0200

    update v5

--- doc/mycode.js
+++ doc/mycode.js
@@ -1,23 +1,3 @@
-
-
-
 MathJax.Hub.Config({
-  //extensions: ['tex2jax.js'],
-  //jax: ['input/TeX', 'output/HTML-CSS'],
-  showMathMenu: false,
-  messageStyle: 'none',
-  //tex2jax: {
-	//inlineMath: inline,
-	//displayMath: block,
-	//processEscapes: true,
-	//# processEnvironments: true,
-	//# preview: 'none',
-	//# skipTags: ["script","noscript","style","textarea"]
-  //},
-  TeX: {
-	//extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-	equationNumbers: { autoNumber: "AMS" }, // <== see this line 
-  },
-  'HTML-CSS': { availableFonts: ['TeX'] },
-  skipStartupTypeset: false
-})
+  TeX: { equationNumbers: { autoNumber: "AMS" } }
+});
